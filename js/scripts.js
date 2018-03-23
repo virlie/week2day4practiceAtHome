f67c21cc9215bce68c2d4bfe2cc17fbfed148dd3
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-22 23:07:22 -0700

    make sure all the functions work.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+  alert ("This is a header");
+    });
+
+  $("p").click(function() {
+  alert ("this is a paragraph");
+  });
+
+  $("img").click(function() {
+  alert("this is an image of the painting.");
+    });
+  });
